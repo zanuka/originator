3673c6bd18613e777feac236c85b4d2eecfd15f1
zanuka <git@zanuka.com>
2015-09-19 22:53:46 -0700

    updates

--- test.js
+++ test.js
@@ -2,13 +2,13 @@ var Snowboard = require('./index.js')
 var test = require('tape')
 
 test('BLACK SNOW snowboard should carve ok', function(assert) {
-  var snowboard = new Snowboard()
+  var snowboard = new Snowboard('BLACK SNOW')
   var result = snowboard.carve()
   assert.equal(result, 'The BLACK SNOW snowboard carves well')
   assert.end()
 })
 
-test('LIB TECH BIRDMAN should carve supremely', function(assert) {
+test('LIB TECH BIRDMAN should carve well', function(assert) {
   var snowboard = new Snowboard('LIB TECH BIRDMAN')
   var result = snowboard.carve()
   assert.equal(result, 'The LIB TECH BIRDMAN snowboard carves well')
