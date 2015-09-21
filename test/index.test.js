74285f769dbc016d381aaacaa9516437c177c8c9
zanuka <git@zanuka.com>
2015-09-20 22:31:06 -0700

    in progress

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,6 @@
 import {Freestyle} from '../src/index'
 import test from 'tape'
 
-test('should return object when passed string', function(assert) {
-  assert.plan(1)
-  const freestyle = new Freestyle('thisName')
-  const result = freestyle.nameObj.name
-  console.log(freestyle.nameObj)
-  // console.log('result', result)
-  assert.equal(result, 'thisName')
-  assert.end()
+test('should write some tests', function(assert) {
+
 })
