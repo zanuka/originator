12eb13cf391f471346bdfd9ac1ab4361a88a280c
zanuka <git@zanuka.com>
2015-10-02 02:04:08 -0700

    fix eslint errors

--- test/index.test.js
+++ test/index.test.js
@@ -2,20 +2,20 @@ import {Originator} from '../src/index'
 import test from 'blue-tape'
 
 test('exports a class', t => {
-  t.equal(typeof Originator, 'function');
-  t.end();
-});
+  t.equal(typeof Originator, 'function')
+  t.end()
+})
 
 test('throws if instantiated improperly', t => {
   t.throws(() => {
-    Originator();
-  });
-  t.end();
-});
+    Originator()
+  })
+  t.end()
+})
 
 test('does not throw if instantiated properly', t => {
   t.doesNotThrow(() => {
-    new Originator();
-  });
-  t.end();
-});
+    new Originator()
+  })
+  t.end()
+})
